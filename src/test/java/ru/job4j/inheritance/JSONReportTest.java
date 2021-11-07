246e1ee4f3f82a08fd1588998a2acf644085405c
Ivan <fufyandiya@gmail.com>
2021-11-07 13:28:21 +0300

    4. Переопределение [#174890]

---
+++ src/test/java/ru/job4j/inheritance/JSONReportTest.java
@@ -0,0 +1,34 @@
+package ru.job4j.inheritance;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class JSONReportTest {
+
+    @Test
+    public void whenTestGenerateMethod() {
+        String ln = System.lineSeparator();
+        String expected = "{" + ln
+                + "\t\"name\" : \"Report's name\"," + ln
+                + "\t\"body\" : \"Report's body\"" + ln
+                + "}";
+        String name = "Report's name";
+        String body = "Report's body";
+        String result = new JSONReport().generate(name, body);
+        assertEquals(expected, result);
+    }
+
+    @Test
+    public void secondWhenTestGenerateMethod() {
+        String ln = System.lineSeparator();
+        String expected = "{" + ln
+                + "\t\"name\" : \"Bla bla\"," + ln
+                + "\t\"body\" : \"bla\"" + ln
+                + "}";
+        String name = "Bla bla";
+        String body = "bla";
+        String result = new JSONReport().generate(name, body);
+        assertEquals(expected, result);
+    }
+}
