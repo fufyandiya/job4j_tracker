df1caa382141dac79645edc11dd2f4ce6d693200
Ivan <fufyandiya@gmail.com>
2021-11-14 15:14:04 +0300

    9. toString [#310018]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -4,9 +4,7 @@ import java.time.format.DateTimeFormatter;
 
 public class StartUI {
     public static void main(String[] args) {
-        Item item = new Item();
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
-        String currentDateTimeFormat = item.getCreated().format(formatter);
-        System.out.println(currentDateTimeFormat);
+        Item item = new Item("item");
+        System.out.println(item);
     }
 }
